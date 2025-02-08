2e83ffd439406e38b2f40d6d3aded9456468145a
jaw <jawkaixuan99@gmail.com>
2025-02-08 22:46:59 +0800

    yay shit works

--- llm/prompt.go
+++ llm/prompt.go
@@ -1,29 +1,6 @@
 package llm
 
 const GENERATE_COMMIT_MESSAGE_PROMPT = `
-You are an expert at following the Conventional Commit specification. 
-
-The expected git commit template is as follow :
-
-    [type1/type2] Title
-    - detail changes 1
-    - detail changes 2
-
-Where type and description is as follow
-
-| Type     | Description                                                                                                                                |
-| -------- | ------------------------------------------------------------------------------------------------------------------------------------------ |
-| feat     | a new feature is introduced with the changes                                                                                               |
-| fix      | a bug fix has occurred                                                                                                                     |
-| chore    | changes that do not relate to a fix or feature and don't modify src or test files (for example updating dependencies)                      |
-| refactor | refactored code that neither fixes a bug nor adds a feature                                                                                |
-| docs     | updates to documentation such as the README or other markdown files                                                                        |
-| style    | changes that do not affect the meaning of the code, likely related to code formatting such as white-space, missing semi-colons, and so on. |
-| test     | including new or correcting previous tests                                                                                                 |
-| perf     | performance improvements                                                                                                                   |
-| ci       | continuous integration related                                                                                                             |
-| build    | changes that affect the build system or external dependencies                                                                              |
-| revert   | reverts a previous commit                                                                                                                  |
 
 Given the git diff listed below, please generate a commit message for me following the rules below STRICTLY because the output will be consumed by another application
 
@@ -33,6 +10,30 @@ Rules:
     2. DON'T wrap the message in code tags
     3. DON'T give any explanation on the commit message
     4. Follow the template closely
+    5. The expected git commit template is as follow :
+
+        [type1/type2] Title
+
+        - detail changes 1
+        - detail changes 2
+
+    feat: a new feature is introduced with the changes
+    fix: a bug fix has occurred
+    chore: changes that do not relate to a fix or feature and don't modify src or test files (for example updating dependencies)
+    refactor: refactored code that neither fixes a bug nor adds a feature
+    docs: updates to documentation such as the README or other markdown files
+    style: changes that do not affect the meaning of the code, likely related to code formatting such as white-space, missing semi-colons, and so on.
+    test: including new or correcting previous tests
+    perf: performance improvements
+    ci: continuous integration related
+    build: changes that affect the build system or external dependencies
+    revert: reverts a previous commit
+
+    EG:
+        [feat/refactor] Display the legends at the bottom of the list 
+
+        - Added legends to the list of items in the commit
+        - Removed the header containing the legends
 
 Code diff:
 %s
--- ui/selectCommit.go
+++ ui/selectCommit.go
@@ -9,18 +9,23 @@ import (
 
 func SelectCommit(commits []string, cursor int) string {
 	var (
-		listWidth = 45
-		boxWidth  = 55
-		listStyle = lipgloss.NewStyle().Width(listWidth).Margin(0, 2)
-		boxStyle  = lipgloss.NewStyle().
+		listWidth = 50
+		boxWidth  = 50
+		boxHeight = calculateMaxHeight(commits, boxWidth-2)
+		listStyle = lipgloss.NewStyle().
+				Width(listWidth).
+				Height(boxHeight).
+				PaddingTop(1)
+		boxStyle = lipgloss.NewStyle().
 				Padding(0, 1).
+				MarginLeft(1).
 				BorderStyle(lipgloss.NormalBorder()).
 				BorderForeground(lipgloss.Color("63"))
 		cursorChar   = "> "
-		legendsStyle = lipgloss.NewStyle().Faint(true).MarginLeft(4)
+		legendsStyle = lipgloss.NewStyle().Faint(true).PaddingRight(1)
 	)
 
-	listItems := make([]string, len(commits)+3)
+	listItems := make([]string, len(commits))
 	for i, commit := range commits {
 		lines := strings.SplitN(commit, "\n", 2)
 		firstLine := lines[0]
@@ -38,8 +43,10 @@ func SelectCommit(commits []string, cursor int) string {
 		}
 	}
 
-	list := lipgloss.JoinVertical(lipgloss.Left, listItems...)
-	boxHeight := calculateMaxHeight(commits, boxWidth)
+	legends := legendsStyle.Render("(↑/↓: arrows, j/k: move, enter: select)")
+
+	list := listStyle.Render(lipgloss.JoinVertical(lipgloss.Left, listItems...))
+	listWithLegends := lipgloss.JoinVertical(lipgloss.Right, list, legends)
 
 	var boxContent string
 
@@ -51,13 +58,10 @@ func SelectCommit(commits []string, cursor int) string {
 
 	box := boxStyle.Width(boxWidth).Height(boxHeight).Render(boxContent)
 
-	header := legendsStyle.Render("(↑/↓: arrows, j/k: move, enter: select)")
-
-	content := lipgloss.JoinHorizontal(lipgloss.Top, listStyle.Render(list), box)
+	content := lipgloss.JoinHorizontal(lipgloss.Top, listWithLegends, box)
 
 	ui := lipgloss.JoinVertical(
 		lipgloss.Left,
-		header,
 		content,
 	)
 
@@ -75,9 +79,11 @@ func calculateMaxHeight(commits []string, maxWidth int) int {
 			lineLen := len(line)
 
 			if lineLen != 0 {
-				commitHeight = commitHeight + int(math.Ceil(float64(maxWidth)/float64(lineLen)))
+				commitHeight = commitHeight + int(math.Ceil(float64(lineLen)/float64(maxWidth)))
+			} else {
+				commitHeight = commitHeight + 1
 			}
-			commitHeight = commitHeight + 1
+
 		}
 
 		if commitHeight > maxHeight {
